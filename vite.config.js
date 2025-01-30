aa624fc4ff4b7c4ce27f15899e70da8620037195
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-01-30 17:13:29 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Todo/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Path to your custom index.html
+      filename: "index.html",
+      inject: "body", // Inject the scripts at the end of the body
+    }),
+  ],
 });
