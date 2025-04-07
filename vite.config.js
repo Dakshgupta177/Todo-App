f43948b2736fae70971c608a696358d7270f02ff
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-04-07 20:11:05 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-Todo-app",
+  base: "/My-todo",
   plugins: [
     react(),
     tailwindcss(),
