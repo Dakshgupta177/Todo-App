6da30ca69e5bcb4ff5f5059c37341b45b83638fe
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-04-10 13:54:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-Todo-app/",
+  base: "/Todo-App/",
   plugins: [
     react(),
     tailwindcss(),
