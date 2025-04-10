c1240d4e5fab3fe2970dde80a276f07a30247cbf
Dakshgupta177 <dakshgupta2k7@gmail.com>
2025-04-10 13:50:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-todo",
+  base: "/My-Todo-app/",
   plugins: [
     react(),
     tailwindcss(),
